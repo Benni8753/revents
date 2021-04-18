981e96e472587f22f17287b80207a00789877fa1
Benjamin Fischer <bfischer549@gmail.com>
2021-04-18 17:47:50 +1200

    End of section 5

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,18 +1,29 @@
-
-import React, { Fragment, useState }from 'react';
-import EventDashboard  from '../../features/events/eventdashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar';
-import { Container } from 'semantic-ui-react';
-
+import React, { Fragment } from "react";
+import EventDashboard from "../../features/events/eventdashboard/EventDashboard";
+import HomePage from "../../features/home/HomePage";
+import NavBar from "../../features/nav/NavBar";
+import EventDetailedPage from "../../features/eventDetailed/EventDetailedPage";
+import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import EventForm from "../../features/events/eventForm/EventForm";
 
 export default function App() {
-  const [formOpen, setFormOpen] = useState(false)
   return (
     <>
-        <NavBar setFormOpen={setFormOpen} />
-        <Container className='main'>
-          <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen} />
-        </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container className="main">
+              <Route exact path="/events" component={EventDashboard} />
+              <Route path="/events/:id" component={EventDetailedPage} />
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
--- src/features/events/eventdashboard/EventDashboard.jsx
+++ src/features/events/eventdashboard/EventDashboard.jsx
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
-import { Grid } from 'semantic-ui-react';
-import EventList from './EventList';
-import EventForm from '../eventForm/EventForm';
-import {sampleData} from '../../../app/api/sampleData';
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard( {formOpen, setFormOpen}) {
-    const [events, setEvents] = useState(sampleData);
+export default function EventDashboard() {
+  const [events, setEvents] = useState(sampleData);
 
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-               <EventList events={events} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {formOpen &&
-                <EventForm setFormOpen={setFormOpen}/>}
-            </Grid.Column>
-        </Grid>
-    )
+  // function handleCreateEvent(event) {
+  //   setEvents([...events, event]);
+  // }
+
+  // function handleUpdateEvent(updatedEvent) {
+  //   setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+  // }
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter(evt => evt.id !== eventId))
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events}  deleteEvent={handleDeleteEvent} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+    <h2>Event Filters</h2>
+      </Grid.Column>
+    </Grid>
+  );
 }
